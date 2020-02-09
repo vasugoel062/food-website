b89fbebec7e231627580815ab30587b4f3d86795
Vasu Goel <vasugoel062@gmail.com>
2020-02-09 20:07:37 +0530

    added some changes

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const userRouter = require("./routers/userRouter");
 const viewRouter = require("./routers/viewRouter");
 const bookingRouter= require("./routers/bookingRouter");
 app.use(cors());
-app.post("/Webhooks-checkout",bookingController.createBooking,bodyParser.raw({type: 'application/json'}));
+app.use(bodyParser.raw({type: 'application/json'}));
+app.post("/Webhooks-checkout",bookingController.createBooking);
 // converts buffer to json
 app.use(express.json());
 // => static files
